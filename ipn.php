b692f870a15286ff8d41543175c755dc53da35f6
Fernando Chamorro <joseferchamorro@gmail.com>
2020-04-24 15:46:38 -0300

    Examen práctico de integración

--- ipn.php
+++ ipn.php
@@ -12,33 +12,12 @@ switch ($_GET["topic"]) {
         $payment = MercadoPago\Payment::find_by_id($_GET["id"]);
         // Get the payment and the corresponding merchant_order reported by the IPN.
         $merchant_order = MercadoPago\MerchantOrder::find_by_id($payment->order->id);
-        print_r($merchant_order);
         break;
     case "merchant_order":
         $merchant_order = MercadoPago\MerchantOrder::find_by_id($_GET["id"]);
         break;
 }
 
-$paid_amount = 0;
-foreach ($merchant_order->payments as $payment) {
-    if ($payment->status == 'approved') {
-        $paid_amount += $payment->transaction_amount;
-    }
-}
-
-// If the payment's transaction amount is equal (or bigger) than the merchant_order's amount you can release your items
-if ($paid_amount >= $merchant_order->total_amount) {
-    if (count($merchant_order->shipments) > 0) { // The merchant_order has shipments
-        if ($merchant_order->shipments[0]->status == "ready_to_ship") {
-            print_r("Totally paid. Print the label and release your item.");
-        }
-    } else { // The merchant_order don't has any shipments
-        print_r("Totally paid. Release your item.");
-    }
-} else {
-    print_r("Not paid yet. Do not release your item.");
-}
-
 date_default_timezone_set("America/Argentina/Buenos_Aires");
-$log  = "Parametros = topic: " . $_GET['topic'] . ' - ' . "id: " . $_GET['id'] . ' - ' . date(DATE_RSS) . PHP_EOL . '+++' . PHP_EOL;
+$log  = "Parametros = topic: " . $_GET['topic'] . ' - id: ' . $_GET['id'] . ' - PreferenceID: ' . $merchant_order->preference_id . ' - Fecha: ' . date(DATE_RSS) . PHP_EOL . '+++' . PHP_EOL;
 file_put_contents('./log_' . date("Ynj") . '.log', $log, FILE_APPEND);
